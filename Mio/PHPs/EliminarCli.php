3addfc899e2b32bc598d2fb66634f070c8c93213
josedavidvitebautista <84037573+josedavidvitebautista@users.noreply.github.com>
2021-12-06 15:30:05 -0600

    The End

--- Mio/PHPs/EliminarCli.php
+++ Mio/PHPs/EliminarCli.php
@@ -6,7 +6,7 @@ $Dato = '';
 if(isset($_POST["FormUno"])){
     $Dato = $_POST["FormUno"];
 }
-$Borrar = "DELETE FROM clientes WHERE Nombre = $Dato";
+$Borrar = "DELETE FROM clientes WHERE Nombre = '$Dato'";
 $queryDel = mysqli_query($Conectar, $Borrar);
 if($queryDel){
     header ('Location: ../HTMLs/Desk.php');
