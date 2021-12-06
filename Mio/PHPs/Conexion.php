c3f5ddc56530eb0b271afaf50396922b9b9befb6
josedavidvitebautista <84037573+josedavidvitebautista@users.noreply.github.com>
2021-12-06 15:02:22 -0600

    End

--- Mio/PHPs/Conexion.php
+++ Mio/PHPs/Conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
-$BD = "proyectoend";
+$user = "id18076419_server";
+$pass = "qwertyuioP24#";
+$BD = "id18076419_prestamos";
 
 $Conectar = mysqli_connect($host, $user, $pass, $BD);
 ?>
