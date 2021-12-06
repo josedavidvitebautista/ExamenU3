20ac4dc898c6853af606353dde0c2f5e9173a581
josedavidvitebautista <84037573+josedavidvitebautista@users.noreply.github.com>
2021-12-06 15:30:43 -0600

    Update EliminarPre.php

--- Mio/PHPs/EliminarPre.php
+++ Mio/PHPs/EliminarPre.php
@@ -6,7 +6,7 @@ $Dato = '';
 if(isset($_POST["FormDos"])){
     $Dato = $_POST["FormDos"];
 }
-$Borrar = "DELETE FROM prestamos WHERE Cliente = $Dato";
+$Borrar = "DELETE FROM prestamos WHERE Cliente = '$Dato'";
 $queryDel = mysqli_query($Conectar, $Borrar);
 if($queryDel){
     header ('Location: ../HTMLs/Desk.php');
