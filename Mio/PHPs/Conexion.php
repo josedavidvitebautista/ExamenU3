f411defb1ac4b20464e5bb7ed508755c87f55e54
Zeiya24 <jose_1320114030@uptecamac.edu.mx>
2021-12-06 14:50:25 -0600

    ...

--- Mio/PHPs/Conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$BD = "proyectoend";
-
-$Conectar = mysqli_connect($host, $user, $pass, $BD);
-?>
--- Mio/PHPs/ConsultaPag.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'Conexion.php';
-
-$Consultar = "SELECT * FROM pagos";
-$queryPag = mysqli_query($Conectar, $Consultar);
-$array = mysqli_fetch_array($queryPag);
